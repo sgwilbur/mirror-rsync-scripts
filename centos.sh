8cd21ef67a18fe2fb80fef339a9b4687ddf4e6bb
Sean Wilbur <sgwilbur@gmail.com>
2011-06-24 13:41:45 -0500

    Updating for 5.6 release

--- centos.sh
+++ centos.sh
@@ -3,18 +3,28 @@
 rsync="/usr/local/bin/rsync -avqHz --delete --delay-updates" # --bwlimit=256"
 mirror=msync.centos.org::CentOS
 
-ver=5
+versionlist="5 5.6"
 archlist="i386 x86_64"
 baselist="os updates"
 local=/var/www/u1/pub/CentOS
 
-for arch in $archlist
-do
-  for base in $baselist
+for ver in $versionlist
   do
-    remote=$mirror/$ver/$base/$arch/
-    echo " Running command: $rsync $remote $local/$ver/$base/$arch/ "
-    $rsync $remote $local/$ver/$base/$arch/
+  for arch in $archlist
+    do
+    for base in $baselist
+      do
+      remote=$mirror/$ver/$base/$arch/
+      echo " Running command: $rsync $remote $local/$ver/$base/$arch/ "
+
+      if [ ! -d $local/$ver/$base/$arch/ ]
+      then
+	echo "Local directory does not exist yet, creating."
+        mkdir -p $local/$ver/$base/$arch/
+      fi
+
+      $rsync $remote $local/$ver/$base/$arch/
+    done
   done
 done
 
